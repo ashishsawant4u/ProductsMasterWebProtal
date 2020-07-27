cfe88201ce8d6c4fa395bbaa65195d2488050aa4
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-28 01:19:33 +0530

    Using Cloud Sleuth

--- src/main/java/com/boot/demo/ProductsPageController.java
+++ src/main/java/com/boot/demo/ProductsPageController.java
@@ -1,5 +1,12 @@
 package com.boot.demo;
 
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,19 +16,30 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class ProductsPageController {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(ProductsPageController.class);
+	
+	
+	@Bean
+	public RestTemplate template() {
+	    return new RestTemplate();
+	}
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@RequestMapping("/products")
 	public String getProducts(Model model)
-	{
+	{	
+		LOG.info("inside PROTAL/getProducts...");
+		
 		model.addAttribute("title", "Products Master Data");
 		
-		String stockAndPriceUrl = "http://localhost:8022/getProducts";
+		String stockAndPriceUrl = "http://localhost:8022/prouctsMS/getProducts";
 		
-		RestTemplate restTemplate = new RestTemplate();
 		
 		ResponseEntity<ProductData[]> resp = restTemplate.getForEntity(stockAndPriceUrl,ProductData[].class);
 		
-		System.out.println("resp from MS >> "+resp.getBody().length);
+		LOG.info("resp from MS >> "+resp.getBody().length);
 		
 		model.addAttribute("products", resp.getBody());
 		
